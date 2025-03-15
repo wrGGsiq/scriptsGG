c0f1fbcf489c38a41173b5c1fd5c64146e6d3b68
wrGGsiq <164651948+wrGGsiq@users.noreply.github.com>
2025-03-15 19:05:55 -0300

    Update Alterar MAC e fazer requisição à API-0.2.meta.js

--- updates/Alterar MAC e fazer requisição à API-0.2.meta.js
+++ updates/Alterar MAC e fazer requisição à API-0.2.meta.js
@@ -1,1 +1,9 @@
-teste
+// ==UserScript==
+// @name         Alterar MAC e fazer requisição à API
+// @version      0.4
+// @description  Altera o MAC e faz uma requisição à API ao identificar a tabela específica
+// @author       Wesley GG
+// @match        https://integrator6.gegnet.com.br/*
+// @license      MIT
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
